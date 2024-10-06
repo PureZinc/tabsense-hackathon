bcf5166a07c5747846f0e446e6702fcba5a36c1a
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-10-06 13:56:10 -0400

    Get Tab Count

--- js/popup.js
+++ js/popup.js
@@ -17,3 +17,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+chrome.tabs.query({}, (tabs) => {
+    const tabCount = tabs.length;
+    document.getElementById('tabCount').textContent = `You have ${tabCount} open tabs.`;
+});
--- js/tabs.js
+++
@@ -1,4 +0,0 @@
-chrome.tabs.query({}, (tabs) => {
-    const tabCount = tabs.length;
-    document.getElementById('tabCount').textContent = `You have ${tabCount} open tabs.`;
-});
