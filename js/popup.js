62e6a4df027c150995b18925f6f10ae2de76c4b5
ShawnBrownDev <shawnbrowndevelopment@gmail.com>
2024-10-06 13:43:10 -0400

    added Settings
    added Popup html and js and finish js
    
    add Service-worker.  js files just need to write the code
    
    Co-Authored-By: zinc_dev <139257470+PureZinc@users.noreply.github.com>

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,19 @@
-document.getElementById('clickMe').addEventListener('click', () => {
-    alert('Button clicked!');
+document.getElementById('saveSettings').addEventListener('click', () => {
+    const timeLimit = parseInt(document.getElementById("timeLimit").value)  * 60 * 1000;
+    const tabLimit = parseInt(document.getElementById("tabLimit").value);
+
+    chrome.storge.local.set({tabTimeLimit: timeLimit, maxTabLimit:tabLimit}, () =>{
+        alert("Setting Saved!");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.storge.local.get(["tabTimeLimit", "maxTabLimit"], (result) => {
+        if (result.tabTimeLimit){
+            document.getElementById("timeLimit").value = result.tabTimeLimit / (60* 1000);
+        }
+        if (result.maxTabLimit){
+            document.getElementById("tabLimit").value = result.maxTabLimit;
+        }
+    });
 });
