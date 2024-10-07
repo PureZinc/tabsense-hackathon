018b9b32e74d5d73a00e5bcc564822869e175c17
ShawnBrownDev <shawnbrowndevelopment@gmail.com>
2024-10-07 16:58:06 -0400

    added the code for Break-timer

--- js/break-timer.js
+++ js/break-timer.js
@@ -1,1 +1,20 @@
-// Remind users to take a Break if they been browsing for a long time code will go here 
+const BREAK_INITERVAL = 3 * 1000;
+
+function startBreakTimer(){
+    setInterval(() => {
+         sendBreakNotification();
+    }, BREAK_INITERVAL);
+}
+
+function sendBreakNotification(){
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: imgURL("assets/dot.png"),
+        title: `Take a Break!`,
+        message: "You've been browsing for an hour. You should take a break and go touch grass",
+        priority: 2,
+    });
+}
+
+chrome.runtime.onStartup.addListener(startBreakTimer);
+chrome.runtime.onInstalled.addListener(startBreakTimer);
