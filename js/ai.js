09e8fd1a3f9dc15911c3fa7aaa46bc5444e7bd6e
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-10-16 21:50:05 -0400

    Added keyword templates scrapped auth

--- js/ai.js
+++ js/ai.js
@@ -1,20 +1,6 @@
 const API_URL = 'http://127.0.0.1:8000/api'; // Replace with hosted backend URL once deployed 
 
-async function getAuthToken() {
-    return await new Promise((resolve, reject) => {
-        chrome.storage.local.get('authToken', (result) => {
-            if (result.authToken) {
-                resolve(result.authToken);
-            } else {
-                reject("No access token found");
-            }
-        });
-    });
-}
-
 async function accessAI(promp) {
-    const authToken = await getAuthToken();  // This is where I run into issues
-    
     const data = {
         content: promp
     }
@@ -24,7 +10,6 @@ async function accessAI(promp) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authToken}`
             },
             credentials: 'include',
             body: JSON.stringify(data)
@@ -41,33 +26,40 @@ async function accessAI(promp) {
     }
 }
 
-async function getChatHistory(userId) { 
-    const data = {
-        user_id: userId
+const keywordContainer = document.getElementById('keyword-container');
+
+function addKeywordTemplate(kw) {
+    const keyword = document.createElement('p');
+    const name = document.createElement('span');
+    name.innerHTML = `${kw.name}: `
+    keyword.appendChild(name);
+    const description = document.createTextNode(kw.description);
+    keyword.appendChild(description);
+    keywordContainer.appendChild(keyword);
+}
+
+async function makeKeywords() {
+    const keywords = await fetch(`${API_URL}/chat/`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+    }) 
+
+    if (!keywords.ok) {
+        throw new Error(`Error: ${keywords.status} ${keywords.statusText}`);
     }
 
-    try {
-        const response = await fetch(`${API_URL}/chat-history/`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include',
-            body: JSON.stringify(data)
-        });
-
-        if (!response.ok) {
-            throw new Error(`Server error: ${response.statusText}`);
-        }
-
-        return await response.json();
-
-    } catch (error) {
-        return { response: 'Failed to connect to the server.' };
+    const data = await keywords.json();
+    console.log(keywords);
+    for (const kw of data.keywords) {
+        addKeywordTemplate(kw);
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const prompted = document.getElementById('input-text');
     const response = document.getElementById('response');
+
+    makeKeywords();
 
     document.getElementById('ask-btn').addEventListener('click', async () => {
         const userInput = prompted.value;
